a1b8016c1235e9ddaa37fdbeb43f49d2264cff28
Joe Liang <joeliang@paypal.com>
2020-10-05 21:49:43 -0700

    joe changes

--- APCSA/src/ch3/BankAccount.java
+++ APCSA/src/ch3/BankAccount.java
@@ -1,54 +1,55 @@
 package ch3;
 
+import java.math.BigDecimal;
+import java.math.BigInteger;
+
 /**
-   A bank account has a balance that can be changed by 
-   deposits and withdrawals.
-*/
-public class BankAccount
-{  
-   private double balance;
+ * A bank account has a balance that can be changed by
+ * deposits and withdrawals.
+ */
+public class BankAccount {
+    private double balance;
 
-   /**
-      Constructs a bank account with a zero balance.
-   */
-   public BankAccount()
-   {   
-      balance = 0;
-   }
+    /**
+     * Constructs a bank account with a zero balance.
+     */
+    public BankAccount() {
+        balance = 0;
+    }
 
-   /**
-      Constructs a bank account with a given balance.
-      @param initialBalance the initial balance
-   */
-   public BankAccount(double initialBalance)
-   {   
-      balance = initialBalance;
-   }
+    /**
+     * Constructs a bank account with a given balance.
+     *
+     * @param initialBalance the initial balance
+     */
+    public BankAccount(double initialBalance) {
+        balance = initialBalance;
+    }
 
-   /**
-      Deposits money into the bank account.
-      @param amount the amount to deposit
-   */
-   public void deposit(double amount)
-   {  
-      balance = balance + amount;
-   }
+    /**
+     * Deposits money into the bank account.
+     *
+     * @param amount the amount to deposit
+     */
+    public void deposit(double amount) {
+        balance = balance + amount;
+    }
 
-   /**
-      Withdraws money from the bank account.
-      @param amount the amount to withdraw
-   */
-   public void withdraw(double amount)
-   {   
-      balance = balance - amount;
-   }
+    /**
+     * Withdraws money from the bank account.
+     *
+     * @param amount the amount to withdraw
+     */
+    public void withdraw(double amount) {
+        balance = balance - amount;
+    }
 
-   /**
-      Gets the current balance of the bank account.
-      @return the current balance
-   */
-   public double getBalance()
-   {   
-      return balance;
-   }
+    /**
+     * Gets the current balance of the bank account.
+     *
+     * @return the current balance
+     */
+    public double getBalance() {
+        return balance;
+    }
 }
